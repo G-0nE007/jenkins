eb4fff5fd072e49483d91a429f12d9dcb3c7dd62
unknown <jeevanmd007@gmail.com>
2025-03-10 10:27:34 +0530

    v1

--- add.java
+++ add.java
@@ -1,7 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
         int num1 = 5; // Static input value 1
-        int num2 = 3; // Static input value 2
+        int num2 = 5; // Static input value 2
         
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
